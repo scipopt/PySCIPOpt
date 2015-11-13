f4f542dbf497d74e7d77c90b23a5c19b08773150
Robert Schwarz <schwarz@zib.de>
2015-11-13 15:50:59 +0100

    add class 'LinExpr'
     - heavily inspired by package "python-algebraic"
     - untested!

---
+++ pyscipopt/linexpr.py
@@ -0,0 +1,55 @@
+CONST = ()
+
+def _is_number(e):
+    try:
+        f = float(e)
+        return True
+    except ValueError:
+        return False
+
+class LinExpr(object):
+    '''Linear expressions of variables with operator overloading.'''
+
+    def __init__(self, terms=None):
+        '''terms is a dict of variables to coefficients.
+
+        The empty tuple is used as key for the constant term.'''
+        self.terms = {} if terms is None else terms
+
+    def __getitem__(self, key):
+        return self.terms[key]
+
+    def __add__(self, other):
+        terms = self.terms.copy()
+        if isinstance(other, LinExpr):
+            # merge the terms by component-wise addition
+            for v,c in other.terms.items():
+                terms[v] = terms.get(v, 0.0) + c
+        elif _is_number(other):
+            c = float(other)
+            terms[CONST] = terms.get(CONST) + c
+        else:
+            raise NotImplementedError
+        return LinExpr(terms)
+
+    def __mul__(self, other):
+        if _is_number(other):
+            f = float(other)
+            return LinExpr({v:f*c for v,c in self.terms.items()})
+        else:
+            raise NotImplementedError
+
+    def __neg__(self):
+        return LinExpr({v:-c for v,c in self.terms.items()})
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __rsub__(self, other):
+        return -1.0 * self + other
