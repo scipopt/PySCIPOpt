e479cbf2ec83eee04e54301e1437c4af9edefa0b
Matthias Miltenberger <miltenberger@zib.de>
2016-04-01 18:04:54 +0200

    correct Python tsp_lazy example

--- examples/scip_book_expressions/tsp_lazy.py
+++ examples/scip_book_expressions/tsp_lazy.py
@@ -11,7 +11,48 @@ Copyright (c) by Joao Pedro PEDROSO and Mikio KUBO, 2012
 import math
 import random
 import networkx
-from pyscipopt import Model, Sepa, quicksum, scip_result
+from pyscipopt import Model, Conshdlr, quicksum, SCIP_RESULT, SCIP_PRESOLTIMING, SCIP_PROPTIMING
+
+class TSPconshdlr(Conshdlr):
+
+
+    def findSubtours(self, checkonly):
+        EPS = 1.e-6
+        edges = []
+        x = self.model.data
+        for (i,j) in x:
+            if self.model.getVal(x[i,j]) > EPS:
+                edges.append((i,j))
+
+        G = networkx.Graph()
+        G.add_edges_from(edges)
+        Components = list(networkx.connected_components(G))
+
+        if len(Components) == 1:
+            return False
+        elif checkonly:
+            return True
+
+        for S in Components:
+            self.model.addCons(quicksum(x[i, j] for i in S for j in S if j > i) <= len(S) - 1)
+            # print("cut: len(%s) <= %s" % (S, len(S) - 1))
+
+        return True
+
+    def conscheck(self, constraints, solution, checkintegrality, checklprows, printreason):
+        if self.findSubtours(checkonly = True):
+            return {"result": SCIP_RESULT.INFEASIBLE}
+        else:
+            return {"result": SCIP_RESULT.FEASIBLE}
+
+    def consenfolp(self, constraints, nusefulconss, solinfeasible):
+        if self.findSubtours(checkonly = False):
+            return {"result": SCIP_RESULT.CONSADDED}
+        else:
+            return {"result": SCIP_RESULT.FEASIBLE}
+
+    def conslock(self, constraint, nlockspos, nlocksneg):
+        pass
 
 def tsp(V,c):
     """tsp -- model for solving the traveling salesman problem with callbacks
@@ -23,54 +64,34 @@ def tsp(V,c):
     Returns the optimum objective value and the list of edges used.
     """
 
-    EPS = 1.e-6
-    def py_sepa_execlp():
-
-        edges = []
-        for (i,j) in x:
-            if model.getVal(x[i,j]) > EPS:
-                edges.append( (i,j) )
-
-        G = networkx.Graph()
-        G.add_edges_from(edges)
-        Components = networkx.connected_components(G)
-
-        if len(Components) == 1:
-            return
-        for S in Components:
-            model.cbLazy(quicksum(x[i,j] for i in S for j in S if j>i) <= len(S)-1)
-            print("cut: len(%s) <= %s" % (S,len(S)-1))
-        return
-
-
-    model = Model("tsp")
+    model = Model("TSP_lazy")
     model.hideOutput()
 
-    sepa = Sepa()
+    conshdlr = TSPconshdlr()
 
     x = {}
     for i in V:
         for j in V:
             if j > i:
-                x[i,j] = model.addVar(vtype="B", name="x(%s,%s)"%(i,j))
+                x[i,j] = model.addVar(vtype = "B",name = "x(%s,%s)" % (i,j))
 
     for i in V:
-        model.addCons(quicksum(x[j,i] for j in V if j < i) + \
-                        quicksum(x[i,j] for j in V if j > i) == 2, "Degree(%s)"%i)
+        model.addCons(quicksum(x[j, i] for j in V if j < i) +
+                      quicksum(x[i, j] for j in V if j > i) == 2, "Degree(%s)" % i)
 
-    model.setObjective(quicksum(c[i,j]*x[i,j] for i in V for j in V if j > i), "minimize")
+    model.setObjective(quicksum(c[i, j] * x[i, j] for i in V for j in V if j > i), "minimize")
 
     model.data = x
-    return model,sepa
+    return model, conshdlr
 
 
 def distance(x1,y1,x2,y2):
     """distance: euclidean distance between (x1,y1) and (x2,y2)"""
-    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 def make_data(n):
     """make_data: compute matrix distance based on euclidean distance"""
-    V = range(1,n+1)
+    V = range(1,n + 1)
     x = dict([(i,random.random()) for i in V])
     y = dict([(i,random.random()) for i in V])
     c = {}
@@ -78,47 +99,48 @@ def make_data(n):
         for j in V:
             if j > i:
                 c[i,j] = distance(x[i],y[i],x[j],y[j])
-    return V,c
+    return V,c,x,y
 
 def solve_tsp(V,c):
-    model,separator = tsp(V,c)
-    model.includeSeparator(separator, "TSP", "TSP subtour eliminator")
+    model, conshdlr = tsp(V, c)
+    model.includeConshdlr(conshdlr, "TSP", "TSP subtour eliminator",
+                          sepapriority = 0, enfopriority = 1, chckpriority = 1, sepafreq = -1, propfreq = -1,
+                          eagerfreq = -1, maxprerounds = 0, delaysepa = False, delayprop = False, needscons = False,
+                          presoltiming = SCIP_PRESOLTIMING.FAST, proptiming = SCIP_PROPTIMING.BEFORELP)
     model.setBoolParam("misc/allowdualreds", 0)
     model.optimize()
+#     model.printStatistics()
     x = model.data
-
-    #if model.status == GRB.Status.TIME_LIMIT:
-        #return None,None
 
     EPS = 1.e-6
     edges = []
     for (i,j) in x:
-        if x[i,j].X > EPS:
-            edges.append( (i,j) )
-    return model.getObjVal(),edges
+        if model.getVal(x[i, j]) > EPS:
+            edges.append((i, j))
+    return model.getObjVal(), edges
 
 if __name__ == "__main__":
     import sys
 
-    # Parse argument
     if len(sys.argv) < 2:
         print("Usage: %s instance" % sys.argv[0])
-        exit(1)
-        # n = 200
-        # seed = 1
-        # random.seed(seed)
-        # V,c = make_data(n)
+        print("using randomly generated TSP instance")
+        n = 200
+        seed = 1
+        random.seed(seed)
+        V,c,x,y = make_data(n)
 
-    from read_tsplib import read_tsplib
-    try:
-        V,c,x,y = read_tsplib(sys.argv[1])
-    except:
-        print("Cannot read TSPLIB file",sys.argv[1])
-        exit(1)
+    else:
+        from read_tsplib import read_tsplib
+        try:
+            V, c, x, y = read_tsplib(sys.argv[1])
+        except:
+            print("Cannot read TSPLIB file", sys.argv[1])
+            exit(1)
 
-    obj,edges = solve_tsp(V,c)
+    obj, edges = solve_tsp(V, c)
 
-    print
-    print("Optimal tour:",edges)
-    print("Optimal cost:",obj)
-    print
+    print("")
+    print("Optimal tour:", edges)
+    print("Optimal cost:", obj)
+    print("")
