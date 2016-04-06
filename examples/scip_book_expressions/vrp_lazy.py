6d09c955af0b7e2a37ebf15d9988d64d780c9d3f
Matthias Miltenberger <miltenberger@zib.de>
2016-04-06 18:48:32 +0200

    correct vehicle routing example from the book

--- examples/scip_book_expressions/vrp_lazy.py
+++ examples/scip_book_expressions/vrp_lazy.py
@@ -10,9 +10,54 @@ Copyright (c) by Joao Pedro PEDROSO and Mikio KUBO, 2012
 import math
 import random
 import networkx
-from pyscipopt import Model, quicksum, multidict
+from pyscipopt import Model, Conshdlr, quicksum, multidict, SCIP_RESULT, SCIP_PRESOLTIMING, SCIP_PROPTIMING
 
-def vrp(V,c,m,q,Q):
+class VRPconshdlr(Conshdlr):
+
+    def addCuts(self, checkonly):
+        """add cuts if necessary and return whether model is feasible"""
+        cutsadded = False
+        edges = []
+        x = self.model.data
+        for (i, j) in x:
+            if self.model.getVal(x[i, j]) > .5:
+                if i != V[0] and j != V[0]:
+                    edges.append((i, j))
+        G = networkx.Graph()
+        G.add_edges_from(edges)
+        Components = list(networkx.connected_components(G))
+        for S in Components:
+            S_card = len(S)
+            q_sum = sum(q[i] for i in S)
+            NS = int(math.ceil(float(q_sum) / Q))
+            S_edges = [(i, j) for i in S for j in S if i < j and (i, j) in edges]
+            if S_card >= 3 and (len(S_edges) >= S_card or NS > 1):
+                cutsadded = True
+                if checkonly:
+                    break
+                else:
+                    self.model.addCons(quicksum(x[i, j] for i in S for j in S if j > i) <= S_card - NS)
+                    print("adding cut for", S_edges)
+
+        return cutsadded
+
+    def conscheck(self, constraints, solution, checkintegrality, checklprows, printreason):
+        if self.addCuts(checkonly = True):
+            return {"result": SCIP_RESULT.INFEASIBLE}
+        else:
+            return {"result": SCIP_RESULT.FEASIBLE}
+
+    def consenfolp(self, constraints, nusefulconss, solinfeasible):
+        if self.addCuts(checkonly = False):
+            return {"result": SCIP_RESULT.CONSADDED}
+        else:
+            return {"result": SCIP_RESULT.FEASIBLE}
+
+    def conslock(self, constraint, nlockspos, nlocksneg):
+        pass
+
+
+def vrp(V, c, m, q, Q):
     """solve_vrp -- solve the vehicle routing problem.
        - start with assignment model (depot has a special status)
        - add cuts until all components of the graph are connected
@@ -24,36 +69,9 @@ def vrp(V,c,m,q,Q):
         - Q: vehicle capacity
     Returns the optimum objective value and the list of edges used.
     """
-    def vrp_callback(model,where):
-        """vrp_callback: add constraint to eliminate infeasible solutions
-        Parameters: gurobi standard:
-            - model: current model
-            - where: indicator for location in the search
-        If solution is infeasible, adds a cut using cbLazy
-        """
-        # remember to set     model.params.DualReductions = 0     before using!
-        if where != GRB.callback.MIPSOL:
-            return
-        edges = []
-        for (i,j) in x:
-            if model.cbGetSolution(x[i,j]) > .5:
-                if i != V[0] and j != V[0]:
-                    edges.append( (i,j) )
-        G = networkx.Graph()
-        G.add_edges_from(edges)
-        Components = networkx.connected_components(G)
-        for S in Components:
-            S_card = len(S)
-            q_sum = sum(q[i] for i in S)
-            NS = int(math.ceil(float(q_sum)/Q))
-            S_edges = [(i,j) for i in S for j in S if i<j and (i,j) in edges]
-            if S_card >= 3 and (len(S_edges) >= S_card or NS > 1):
-                model.cbLazy(quicksum(x[i,j] for i in S for j in S if j > i) <= S_card-NS)
-                print("adding cut for",S_edges)
-        return
-
 
     model = Model("vrp")
+    vrp_conshdlr = VRPconshdlr()
 
     x = {}
     for i in V:
@@ -69,51 +87,51 @@ def vrp(V,c,m,q,Q):
                         quicksum(x[i,j] for j in V if j > i) == 2, "Degree(%s)"%i)
 
     model.setObjective(quicksum(c[i,j]*x[i,j] for i in V for j in V if j>i), "minimize")
-
     model.data = x
-    return model,vrp_callback
+
+    return model, vrp_conshdlr
 
 
 def distance(x1,y1,x2,y2):
     """distance: euclidean distance between (x1,y1) and (x2,y2)"""
-    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 def make_data(n):
     """make_data: compute matrix distance based on euclidean distance"""
     V = range(1,n+1)
-    x = dict([(i,random.random()) for i in V])
-    y = dict([(i,random.random()) for i in V])
-    c,q = {},{}
+    x = dict([(i, random.random()) for i in V])
+    y = dict([(i, random.random()) for i in V])
+    c, q = {}, {}
     Q = 100
     for i in V:
-        q[i] = random.randint(10,20)
+        q[i] = random.randint(10, 20)
         for j in V:
             if j > i:
-                c[i,j] = distance(x[i],y[i],x[j],y[j])
-    return V,c,q,Q
+                c[i, j] = distance(x[i], y[i], x[j], y[j])
+    return V, c, q, Q
 
 
 if __name__ == "__main__":
-    import sys
-
     n = 19
     m = 3
     seed = 1
     random.seed(seed)
     V,c,q,Q = make_data(n)
-    model,vrp_callback = vrp(V,c,m,q,Q)
+    model, conshdlr = vrp(V, c, m, q, Q)
 
-    # model.Params.OutputFlag = 0 # silent mode
-    #model.params.DualReductions = 0
     model.setBoolParam("misc/allowdualreds", 0)
-    model.optimize(vrp_callback)
+    model.includeConshdlr(conshdlr, "VRP", "VRP constraint handler",
+                          sepapriority = 0, enfopriority = 1, chckpriority = 1, sepafreq = -1, propfreq = -1,
+                          eagerfreq = -1, maxprerounds = 0, delaysepa = False, delayprop = False, needscons = False,
+                          presoltiming = SCIP_PRESOLTIMING.FAST, proptiming = SCIP_PROPTIMING.BEFORELP)
+    model.optimize()
     x = model.data
 
     edges = []
-    for (i,j) in x:
-        if x[i,j].X > .5:
+    for (i, j) in x:
+        if model.getVal(x[i, j]) > .5:
             if i != V[0] and j != V[0]:
-                edges.append( (i,j) )
+                edges.append((i, j))
 
     print("Optimal solution:", model.getObjVal())
     print("Edges in the solution:")
