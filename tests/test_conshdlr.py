4aff6305016ee26cf99e8bf2bf4b14329cdc09ea
Matthias Miltenberger <miltenberger@zib.de>
2016-05-02 19:30:51 +0200

    try to test all callbacks in Python conshdlr

--- tests/test_conshdlr.py
+++ tests/test_conshdlr.py
@@ -1,33 +1,130 @@
 from pyscipopt import Model, Conshdlr, SCIP_RESULT, SCIP_PRESOLTIMING, SCIP_PROPTIMING
 from types import SimpleNamespace
 
+ids = []
+
 class MyConshdlr(Conshdlr):
+
     def createData(self, constraint, nvars, othername):
         print("Creating data for my constraint: %s"%constraint.name)
         constraint.data = SimpleNamespace()
         constraint.data._nvars = nvars
         constraint.data._myothername = othername
 
-
     def consenfolp(self, constraints, nusefulconss, solinfeasible):
-        print("consenforcinglp python style")
-        for cons in constraints:
-            if cons.data._nvars > 0:
-                result = SCIP_RESULT.FEASIBLE
-                print(cons.data._myothername, "is feasible while enforcing")
-        return {"result": result}
+        print("[consenfolp]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+        return {"result": SCIP_RESULT.FEASIBLE}
 
     def conscheck(self, constraints, solution, checkintegrality, checklprows, printreason):
-        print("conscheking python style")
-        for cons in constraints:
-            if cons.data._nvars > 0:
-                result = SCIP_RESULT.FEASIBLE
-                print(cons.data._myothername, "is so feasible...")
-        return {"result": result}
+        print("[conscheck]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+        return {"result": SCIP_RESULT.FEASIBLE}
 
-    def conslock(self, constraints, nlockspos, nlocksneg):
-        print("don't care about locks.. don't even have a variable!")
-        pass
+    def conslock(self, constraint, nlockspos, nlocksneg):
+        print("[conslock]")
+        assert id(constraint) in ids
+
+    def constrans(self, sourceconstraint):
+        print("[constrans]")
+        assert id(sourceconstraint) in ids
+        return {}
+
+    def consprop(self, constraints, nusefulconss, nmarkedconss, proptiming):
+        print("[consprop]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+        return {}
+
+    def conssepalp(self, constraints, nusefulconss):
+        print("[conssepalp]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+        return {}
+
+    def conssepasol(self, constraints, nusefulconss, solution):
+        print("[conssepasol]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+        return {}
+
+    def conspresol(self, constraints, nrounds, presoltiming,
+                   nnewfixedvars, nnewaggrvars, nnewchgvartypes, nnewchgbds, nnewholes,
+                   nnewdelconss, nnewaddconss, nnewupgdconss, nnewchgcoefs, nnewchgsides, result_dict):
+        print("[conspresol]")
+        return result_dict
+
+    def consdelete(self, constraint):
+        print("[consdelete]")
+        assert id(constraint) in ids
+
+    def consinit(self, constraints):
+        print("[consinit]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+
+    def consexit(self, constraints):
+        print("[consexit]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+
+    def consinitpre(self, constraints):
+        print("[consinitpre]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+
+    def consexitpre(self, constraints):
+        print("[consexitpre]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+
+    def consinitsol(self, constraints):
+        print("[consinitsol]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+
+    def consexitsol(self, constraints, restart):
+        print("[consexitsol]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+
+    def consinitlp(self, constraints):
+        print("[consinitlp]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+        return {}
+
+    def consactive(self, constraint):
+        print("[consactive]")
+        assert id(constraint) in ids
+
+    def consdeactive(self, constraint):
+        print("[consdeactive]")
+        assert id(constraint) in ids
+
+    def consenable(self, constraint):
+        print("[consenable]")
+        assert id(constraint) in ids
+
+    def consdisable(self, constraint):
+        print("[consdisable]")
+        assert id(constraint) in ids
+
+    def consdelvars(self, constraints):
+        print("[consdelvars]")
+        for constraint in constraints:
+            assert id(constraint) in ids
+
+    def consprint(self, constraint):
+        print("[consprint]")
+        assert id(constraint) in ids
+
+    def consgetnvars(self, constraint):
+        print("[consgetnvars]")
+        assert id(constraint) in ids
+        return {}
 
 
 def test_conshdlr():
@@ -37,21 +134,25 @@ def test_conshdlr():
         s = Model()
 
         # add some variables
-        x = s.addVar("x", obj=1.0)
-        y = s.addVar("y", obj=2.0)
+        x = s.addVar("x", obj = 1.0, vtype = "I")
+        y = s.addVar("y", obj = 2.0, vtype = "I")
 
         # add some constraint
         s.addCons(x + 2*y >= 5)
+        s.addCons(x + 2.1 * y >= 5)
+        s.addCons(x + 1.9 * y >= 5)
 
         # create conshdlr and include it to SCIP
         conshdlr = MyConshdlr()
         s.includeConshdlr(conshdlr, "PyCons", "custom constraint handler implemented in python",
-                              sepapriority=0, enfopriority=0, chckpriority=0, sepafreq=1, propfreq=-1,
-                              eagerfreq=-1, maxprerounds=0, delaysepa=False, delayprop=False, needscons=True,
-                              presoltiming=SCIP_PRESOLTIMING.FAST, proptiming=SCIP_PROPTIMING.BEFORELP)
+                          sepapriority = 1, enfopriority = 1, chckpriority = 1, sepafreq = 1, propfreq = 1,
+                          eagerfreq = 1, maxprerounds = -1, delaysepa = False, delayprop = False, needscons = True,
+                          presoltiming = SCIP_PRESOLTIMING.FAST, proptiming = SCIP_PROPTIMING.BEFORELP)
 
         cons1 = s.createCons(conshdlr, "cons1name")
+        ids.append(id(cons1))
         cons2 = s.createCons(conshdlr, "cons2name")
+        ids.append(id(cons2))
         conshdlr.createData(cons1, 10, "cons1_anothername")
         conshdlr.createData(cons2, 12, "cons2_anothername")
 
