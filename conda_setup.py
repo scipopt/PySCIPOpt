afdb45268f4b7b4ea87327844a37261f21a5dfb7
Robert Schwarz <mail@rschwarz.net>
2016-05-26 14:26:05 +0200

    add first conda recipe for PySCIPOpt
     - requires the conda package `scipoptlib`
     - uses the non-standard `conda_setup.py` to simplify dependencies
       (no GMP, no readline)

---
+++ conda_setup.py
@@ -0,0 +1,23 @@
+# simplified version of setup.py for conda recipe
+from distutils.core import setup
+from distutils.extension import Extension
+
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+
+extensions = [Extension('pyscipopt.scip', ['pyscipopt/scip.pyx'],
+                        extra_compile_args=['-UNDEBUG'],
+                        include_dirs=['lib/scip-src'],
+                        library_dirs=['lib'],
+                        libraries=['m', 'scipopt', 'z'])]
+
+setup(
+    name = 'pyscipopt',
+    version = '0.0',
+    description = 'wrapper for SCIP in Python',
+    author = 'Zuse Institute Berlin',
+    author_email = 'scip@zib.de',
+    license = 'ZIB',
+    ext_modules = cythonize(extensions),
+    packages=['pyscipopt']
+)
