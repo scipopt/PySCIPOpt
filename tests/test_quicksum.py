7f84a5b8e3c9e202fea99cda50744d4d38b70ada
Robert Schwarz <mail@rschwarz.net>
2016-07-21 12:05:02 +0200

    add test with quicksum for quadratic

--- tests/test_quicksum.py
+++ tests/test_quicksum.py
@@ -11,3 +11,19 @@ def test_quicksum():
     s =      sum([x,y,z,c]) == 0.0
 
     assert(q.expr.terms == s.expr.terms)
+
+def test_largequadratic():
+    # inspired from performance issue on
+    # http://stackoverflow.com/questions/38434300
+
+    m = Model("dense_quadratic")
+    dim = 200
+    x = [m.addVar("x_%d" % i) for i in range(dim)]
+    expr = quicksum((i+j+1)*x[i]*x[j]
+                    for i in range(dim)
+                    for j in range(dim))
+    cons = expr <= 1.0
+    #                              upper triangle,     diagonal
+    assert len(cons.expr.terms) == dim * (dim-1) / 2 + dim
+    m.addCons(cons)
+    # TODO: what can we test beyond the lack of crashes?
