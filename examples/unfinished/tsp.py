dd49a0508d47349cb87d17cdb9da623a8899a597
Matthias Miltenberger <miltenberger@zib.de>
2017-01-11 12:51:54 +0100

    clean up tsp.py

--- examples/unfinished/tsp.py
+++ examples/unfinished/tsp.py
@@ -5,14 +5,16 @@ minimize the travel cost for visiting n customers exactly once
 approach:
     - start with assignment model
     - add cuts until there are no sub-cycles
+    - two cutting plane possibilities (called inside "solve_tsp"):
+        - addcut: limit the number of edges in a connected component S to |S|-1
+        - addcut2: require the number of edges between two connected component to be >= 2
 
 Copyright (c) by Joao Pedro PEDROSO and Mikio KUBO, 2012
 """
 import math
-import time
 import random
 import networkx
-from pyscipopt import Model, quicksum, multidict
+from pyscipopt import Model, quicksum
 
 def solve_tsp(V,c):
     """solve_tsp -- solve the traveling salesman problem
@@ -27,9 +29,10 @@ def solve_tsp(V,c):
     def addcut(cut_edges):
         G = networkx.Graph()
         G.add_edges_from(cut_edges)
-        Components = networkx.connected_components(G)
+        Components = list(networkx.connected_components(G))
         if len(Components) == 1:
             return False
+        model.freeTransform()
         for S in Components:
             model.addCons(quicksum(x[i,j] for i in S for j in S if j>i) <= len(S)-1)
             print("cut: len(%s) <= %s" % (S,len(S)-1))
@@ -39,29 +42,24 @@ def solve_tsp(V,c):
     def addcut2(cut_edges):
         G = networkx.Graph()
         G.add_edges_from(cut_edges)
-        Components = networkx.connected_components(G)
+        Components = list(networkx.connected_components(G))
 
         if len(Components) == 1:
             return False
+        model.freeTransform()
         for S in Components:
             T = set(V) - set(S)
             print("S:",S)
             print("T:",T)
-            model.addCons(quicksum(x[i,j] for i in S for j in T if j>i)
-                          + quicksum(x[i,j] for i in T for j in S if j>i) >= 2)
-            print("cut: %s <--> %s >= 2" % (S,T), [(i,j) for i in S for j in T if j>i])
-        return True
-
-    def isMIP(x):
-        for var in x:
-            if var.vtype == "CONTINUOUS":
-                return False
+            model.addCons(quicksum(x[i,j] for i in S for j in T if j>i) +
+                          quicksum(x[i,j] for i in T for j in S if j>i) >= 2)
+            print("cut: %s >= 2" % "+".join([("x[%s,%s]" % (i,j)) for i in S for j in T if j>i]))
         return True
 
     # main part of the solution process:
     model = Model("tsp")
 
-    # model.Params.OutputFlag = 0 # silent/verbose mode
+    model.hideOutput() # silent/verbose mode
     x = {}
     for i in V:
         for j in V:
@@ -75,6 +73,7 @@ def solve_tsp(V,c):
     model.setObjective(quicksum(c[i,j]*x[i,j] for i in V for j in V if j > i), "minimize")
 
     EPS = 1.e-6
+    isMIP = False
     while True:
         model.optimize()
         edges = []
@@ -82,12 +81,13 @@ def solve_tsp(V,c):
             if model.getVal(x[i,j]) > EPS:
                 edges.append( (i,j) )
 
-        model.freeTransform()
         if addcut(edges) == False:
-            if isMIP():     # integer variables, components connected: solution found
+            if isMIP:     # integer variables, components connected: solution found
                 break
+            model.freeTransform()
             for (i,j) in x:     # all components connected, switch to integer model
                 model.chgVarType(x[i,j], "B")
+                isMIP = True
 
     return model.getObjVal(),edges
 
@@ -115,22 +115,20 @@ if __name__ == "__main__":
     # Parse argument
     if len(sys.argv) < 2:
         print("Usage: %s instance" % sys.argv[0])
-        exit(1)
-        # n = 200
-        # seed = 1
-        # random.seed(seed)
-        # V,c = make_data(n)
-
-    from read_tsplib import read_tsplib
-    try:
-        V,c,x,y = read_tsplib(sys.argv[1])
-    except:
-        print("Cannot read TSPLIB file",sys.argv[1])
-        exit(1)
+        print("Using randomized example instead")
+        n = 200
+        seed = 1
+        random.seed(seed)
+        V,c = make_data(n)
+    else:
+        from read_tsplib import read_tsplib
+        try:
+            V,c,x,y = read_tsplib(sys.argv[1])
+        except:
+            print("Cannot read TSPLIB file",sys.argv[1])
+            exit(1)
 
     obj,edges = solve_tsp(V,c)
 
-    print
-    print("Optimal tour:",edges)
+    print("\nOptimal tour:",edges)
     print("Optimal cost:",obj)
-    print
