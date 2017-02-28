4eab2becc9d2410c0bccf20e66250b5b5ee2d2c5
Matthias Miltenberger <miltenberger@zib.de>
2017-02-28 10:20:11 +0100

    allow querying of solution data in more stages
    Previously, it was not possible to call these methods
    when SCIP was stopped due to e.g. a time limit (stage: SOLVING)

--- pyscipopt/scip.pyx
+++ pyscipopt/scip.pyx
@@ -1253,7 +1253,7 @@ cdef class Model:
         Keyword arguments:
         original -- objective value in original or transformed space (default True)
         """
-        if not self.getStage() == SCIP_STAGE_SOLVED:
+        if not self.getStage() >= SCIP_STAGE_SOLVING:
             raise Warning("method cannot be called before problem is solved")
         return self.getSolObjVal(self._bestSol, original)
 
@@ -1276,7 +1276,7 @@ cdef class Model:
         Keyword arguments:
         var -- the variable to query the value of
         """
-        if not self.getStage() == SCIP_STAGE_SOLVED:
+        if not self.getStage() >= SCIP_STAGE_SOLVING:
             raise Warning("method cannot be called before problem is solved")
         return self.getSolVal(self._bestSol, var)
 
