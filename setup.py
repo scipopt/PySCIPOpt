2b19ba73fe776a08e68644a2e5bff94202723534
Matthias Miltenberger <miltenberger@zib.de>
2017-06-09 14:36:45 +0200

    increase version number to 1.1.2

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if cythonize:
 
 setup(
     name = 'PySCIPOpt',
-    version = '1.1.1',
+    version = '1.1.2',
     description = 'Python interface and modeling environment for SCIP',
     url = 'https://github.com/SCIP-Interfaces/PySCIPOpt',
     author = 'Zuse Institute Berlin',
