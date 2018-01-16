e62ede208696e88d4dd92af0b8361d2bdda3ba9a
Matthias Miltenberger <miltenberger@zib.de>
2018-01-16 15:40:39 +0100

    modify filename handling to fix appveyor errors

--- tests/test_solutionIO.py
+++ tests/test_solutionIO.py
@@ -2,6 +2,7 @@ from pyscipopt import Model
 import pytest
 from pytest import approx
 import re
+import os
 
 def create_model_with_one_optimum():
     m = Model()
@@ -12,7 +13,7 @@ def create_model_with_one_optimum():
     return m
 
 def parse_solutionfile(filename):
-    with open(str(filename), "r") as f:
+    with open(filename, "r") as f:
         lines = f.readlines()
 
     solobj = lines[0].split(":")[1].strip()
@@ -21,7 +22,7 @@ def parse_solutionfile(filename):
     for line in lines[1:]:
         match = pattern.match(line)
         if match is None:
-            raise Exception("unexpected or mallformated line in solution file:\n%s" % line)
+            raise Exception("unexpected or malformated line in solution file:\n%s" % line)
         d = match.groupdict()
         variables[d['name'].strip()] = float(d['value']), float(d['objectiv'])
     return solobj, variables
@@ -57,90 +58,90 @@ def assert_equal_solutiondata(current, expected):
         if name in variables:
             exp_val, exp_obj = variables[name]
             assert val == approx(exp_val), \
-                "variable '%s' has wrong value: expected %s got %s" % (name, val, exp_val)
+                "variable {} has wrong value: expected {} got {}".format(name, val, exp_val)
             assert obj == approx(exp_obj), \
-                "variable '%s' has wrong objectiv: expected %s got %s" % (name, obj, exp_obj)
+                "variable {} has wrong objectiv: expected {} got {}".format(name, obj, exp_obj)
         else:
             assert val == approx(0), \
-                "variable '%s' was not in solutionfile even so its value is '%s' and not zero" % (name, val)
+                "variable {} was not in solutionfile even so its value is {} and not zero".format(name, val)
 
 
-def test_writeBestSol(tmpdir):
+def test_writeBestSol():
     model = create_model_with_one_optimum()
     model.optimize()
     assert model.getStatus() == "optimal", "model could not be optimized"
 
-    solfile = tmpdir.join("x.sol")
-    model.writeBestSol(str(solfile))
-    assert solfile.exists(), "no solution file was written"
+    solfile = "x.sol"
+    model.writeBestSol(solfile)
+    assert os.path.exists(solfile), "no solution file was written"
 
     sol = model.getBestSol()
     validate_solutionfile(solfile, model, sol)
 
 
-def test_writeAllSol(tmpdir):
+def test_writeAllSol():
     model = create_model_with_one_optimum()
     model.optimize()
     assert model.getStatus() == "optimal", "model could not be optimized"
 
     for idx, sol in enumerate(model.getSols()):
-        solfile = tmpdir.join("x_%d.sol" % idx)
-        model.writeSol(sol, str(solfile))
-        assert solfile.exists() , "no solution file was written"
+        solfile = "x_{}.sol".format(idx)
+        model.writeSol(sol, solfile)
+        assert os.path.exists(solfile) , "no solution file was written"
         validate_solutionfile(solfile, model, sol)
 
-def test_readSolFile(tmpdir):
+def test_readSolFile():
     model = create_model_with_one_optimum()
     model.optimize()
     assert model.getStatus() == "optimal", "model could not be optimized"
 
-    solfile = tmpdir.join("x.sol")
-    model.writeBestSol(str(solfile))
-    assert solfile.exists(), "no solution file was written"
+    solfile = "x.sol"
+    model.writeBestSol(solfile)
+    assert os.path.exists(solfile), "no solution file was written"
 
     exp_sol = model.getBestSol()
 
     model2 = create_model_with_one_optimum()
-    sol = model2.readSolFile(str(solfile))
+    sol = model2.readSolFile(solfile)
 
     assert_equal_solutions(model2, sol, model, exp_sol)
 
 
-def test_useReadSol(tmpdir):
+def test_useReadSol():
     model = create_model_with_one_optimum()
     model.optimize()
     assert model.getStatus() == "optimal", "model could not be optimized"
 
-    solfile = tmpdir.join("x.sol")
-    model.writeBestSol(str(solfile))
-    assert solfile.exists(), "no solution file was written"
+    solfile = "x.sol"
+    model.writeBestSol(solfile)
+    assert os.path.exists(solfile), "no solution file was written"
 
-    statfile = tmpdir.join("x.stats")
-    model.writeStatistics(str(statfile))
-    assert statfile.exists(), "no statistics file was written"
+    statfile = "x.stats"
+    model.writeStatistics(statfile)
+    assert os.path.exists(statfile), "no statistics file was written"
 
     # second model instance
     model2 = create_model_with_one_optimum()
     assert len(model2.getSols()) == 0
 
-    sol = model2.readSolFile(str(solfile))
+    sol = model2.readSolFile(solfile)
     model2.addSol(sol)
     assert len(model2.getSols()) == 1
 
     model2.optimize()
 
-    statfile2 = tmpdir.join("x2.stats")
-    model2.writeStatistics(str(statfile2))
-    assert statfile2.exists(), "no statistics file was written"
+    statfile2 = "x2.stats"
+    model2.writeStatistics(statfile2)
+    assert os.path.exists(statfile2), "no statistics file was written"
 
     pattern = re.compile(r"Solutions\sfound\s+:\s+(\d+)\s+\((\d+)\s+improvements\)")
-    with open(str(statfile), "r") as f1:
-        with open(str(statfile2), "r") as f2:
+    with open(statfile, "r") as f1:
+        with open(statfile2, "r") as f2:
             match1 = pattern.search(f1.read())
             match2 = pattern.search(f2.read())
             assert match1 is not None and match2 is not None, "statistics files are incomplete"
             assert match1.group(1,2) != match2.group(1,2) , \
-                "the number of solutions and improvements should be differing"
+                "the number of solutions and improvements should be different"
 
 #TODO: implement these
 
