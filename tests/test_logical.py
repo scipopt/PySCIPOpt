85f3198e1c61e07baaa708aae9014b1b8634c725
Matthias Miltenberger <miltenberger@zib.de>
2018-06-18 13:00:46 +0200

    clean up test_logical according to pep8 format

--- tests/test_logical.py
+++ tests/test_logical.py
@@ -9,8 +9,6 @@ except ImportError:
 product = itertools.product
 
 
-################################################################################
-#
 # Testing AND/OR/XOR constraints
 #   check whether any error is raised
 # see http://scip.zib.de/doc-5.0.1/html/cons__and_8c.php
@@ -19,30 +17,32 @@ product = itertools.product
 #   Integer and continous variables behave unexpectedly (due to SCIP?)
 # TBI: automatic assertion of expected resultant VS optimal resultant
 #   (visual inspection at the moment)
-#
-################################################################################
 
-verbose = True ### py.test ignores this
+verbose = True  # py.test ignores this
 
-### AUXILIARY FUNCTIONS ###
+
+# AUXILIARY FUNCTIONS
 def setModel(vtype="B", name=None, imax=2):
     """initialize model and its variables.
     imax (int): number of operators"""
-    if name is None: name = "model"
+    if name is None:
+        name = "model"
     m = Model(name)
     m.hideOutput()
     i = 0
     r = m.addVar("r", vtype)
     while i < imax:
         m.addVar("v%s" % i, vtype)
-        i+=1
+        i += 1
     return m
+
 
 def getVarByName(m, name):
     try:
         return [v for v in m.getVars() if name == v.name][0]
     except IndexError:
         return None
+
 
 def getAllVarsByName(m, name):
     try:
@@ -57,21 +57,23 @@ def setConss(m, vtype="B", val=0, imax=1):
     imax (int): number of operators affected by the constraint"""
     i = 0
     while i < imax:
-        vi = getVarByName(m,"v%s" % i)
+        vi = getVarByName(m, "v%s" % i)
         m.addCons(vi == val, vtype)
-        i+=1
+        i += 1
     return
+
 
 def printOutput(m):
     """print status and values of variables AFTER optimization."""
     status = m.getStatus()
-    r = getVarByName(m,"r")
+    r = getVarByName(m, "r")
     rstr = "%d" % round(m.getVal(r))
     vs = getAllVarsByName(m, "v")
     vsstr = "".join(["%d" % round(m.getVal(v)) for v in vs])
     print("Status: %s, resultant: %s, operators: %s" % (status, rstr, vsstr))
 
-### MAIN FUNCTIONS ###
+
+# MAIN FUNCTIONS
 def main_variable(model, logical, sense="min"):
     """r is the BINARY resultant variable
     v are BINARY operators
@@ -79,25 +81,28 @@ def main_variable(model, logical, sense="min"):
     try:
         r = getVarByName(model, "r")
         vs = getAllVarsByName(model, "v")
-        ### addConsAnd/Or method (Xor: TBI, custom) ###
+        # addConsAnd/Or method (Xor: TBI, custom) ###
         method_name = "addCons%s" % logical.capitalize()
         if method_name == "addConsXor":
-            n = model.addVar("n","I")
+            n = model.addVar("n", "I")
             model.addCons(r+quicksum(vs) == 2*n)
         else:
             try:
                 _model_addConsLogical = getattr(model, method_name)
-                _model_addConsLogical(vs,r)
+                _model_addConsLogical(vs, r)
             except AttributeError as e:
                 raise AttributeError("%s not implemented" % method_name)
         model.setObjective(r, sense="%simize" % sense)
         model.optimize()
         assert model.getStatus() == "optimal"
-        if verbose: printOutput(model)
+        if verbose:
+            printOutput(model)
         return True
     except Exception as e:
-        if verbose: print("%s: %s" % (e.__class__.__name__, e))
+        if verbose:
+            print("%s: %s" % (e.__class__.__name__, e))
         return False
+
 
 def main_boolean(model, logical, value=False):
     """r is the BOOLEAN rhs (NOT a variable!)
@@ -106,54 +111,58 @@ def main_boolean(model, logical, value=False):
     try:
         r = value
         vs = getAllVarsByName(model, "v")
-        ### addConsXor method (And/Or: TBI) ###
+        # addConsXor method (And/Or: TBI) ###
         method_name = "addCons%s" % logical.capitalize()
         try:
             _model_addConsLogical = getattr(model, method_name)
-            _model_addConsLogical(vs,r)
+            _model_addConsLogical(vs, r)
         except AttributeError as e:
             raise AttributeError("%s not implemented" % method_name)
         model.optimize()
         assert model.getStatus() == "optimal"
-        if verbose: printOutput(model)
+        if verbose:
+            printOutput(model)
         return True
     except Exception as e:
-        if verbose: print("%s: %s" % (e.__class__.__name__, e))
+        if verbose:
+            print("%s: %s" % (e.__class__.__name__, e))
         return False
 
-### TEST FUNCTIONS ###
+
+# TEST FUNCTIONS
 @pytest.mark.parametrize("nconss", [1, 2, "all"])
 @pytest.mark.parametrize("vconss", [0, 1])
-@pytest.mark.parametrize("sense", ["min","max"])
+@pytest.mark.parametrize("sense", ["min", "max"])
 @pytest.mark.parametrize("logical", ["and", "or", "xor"])
-@pytest.mark.parametrize("noperators", [2,20,51,100])
+@pytest.mark.parametrize("noperators", [2, 20, 51, 100])
 @pytest.mark.parametrize("vtype", ["B"])
 def test_variable(noperators, vtype, logical, sense, vconss, nconss):
     if nconss == "all":
         nconss = noperators
-    if vtype in ["I","C"]:
+    if vtype in ["I", "C"]:
         pytest.skip("unsupported vtype \"%s\" may raise errors or unexpected results" % vtype)
     m = setModel(vtype, logical, noperators)
-    setConss(m,vtype, vconss, nconss)
+    setConss(m, vtype, vconss, nconss)
     success = main_variable(m, logical, sense)
     assert(success), "Status is not optimal"
+
 
 @pytest.mark.parametrize("nconss", [1, 2, "all"])
 @pytest.mark.parametrize("vconss", [0, 1])
 @pytest.mark.parametrize("value", [False, True])
-@pytest.mark.parametrize("logical", ["xor","and", "or"])
-@pytest.mark.parametrize("noperators", [2,20,51,100])
+@pytest.mark.parametrize("logical", ["xor", "and", "or"])
+@pytest.mark.parametrize("noperators", [2, 20, 51, 100])
 @pytest.mark.parametrize("vtype", ["B"])
 def test_boolean(noperators, vtype, logical, value, vconss, nconss):
     if nconss == "all":
         nconss = noperators
-    if vtype in ["I","C"]:
+    if vtype in ["I", "C"]:
         pytest.skip("unsupported vtype \"%s\" may raise errors or unexpected results" % vtype)
-    if logical in ["and","or"]:
+    if logical in ["and", "or"]:
         pytest.skip("unsupported logical: %s" % vtype)
-    if logical == "xor" and nconss == noperators and noperators%2 & vconss != value:
+    if logical == "xor" and nconss == noperators and noperators % 2 & vconss != value:
         pytest.xfail("addConsXor cannot be %s if an %s number of variables are all constraint to %s" % (value, noperators, vconss))
     m = setModel(vtype, logical, noperators)
-    setConss(m,vtype, vconss, nconss)
+    setConss(m, vtype, vconss, nconss)
     success = main_boolean(m, logical, value)
     assert(success), "Test is not successful"
