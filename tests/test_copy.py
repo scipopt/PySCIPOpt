290d5e1b671376b749bfade39dfc96b63ae0d87f
Matthias Miltenberger <miltenberger@zib.de>
2019-03-15 11:43:43 +0100

    remove obj offset from copy test

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -7,7 +7,7 @@ def test_copy():
     # add some variables
     x = s.addVar("x", vtype = 'C', obj = 1.0)
     y = s.addVar("y", vtype = 'C', obj = 2.0)
-    s.setObjective(4.0 * y + 10.5, clear = False)
+    s.setObjective(4.0 * y, clear = False)
 
     c = s.addCons(x + 2 * y >= 1.0)
 
