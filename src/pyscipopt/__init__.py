4200c5e7e8f1a0edee1ee6135541d83449788462
Matthias Miltenberger <matthias.miltenberger@gmail.com>
2020-02-12 22:22:15 +0100

    fix typo in path join call

--- src/pyscipopt/__init__.py
+++ src/pyscipopt/__init__.py
@@ -3,7 +3,7 @@ __version__ = '2.2.3'
 # required for Python 3.8 on Windows
 import os
 if hasattr(os, 'add_dll_directory'):
-    os.add_dll_directory(os.path.join(os.getenv('SCIPOPTDIR', 'bin')))
+    os.add_dll_directory(os.path.join(os.getenv('SCIPOPTDIR'), 'bin'))
 
 # export user-relevant objects:
 from pyscipopt.Multidict import multidict
