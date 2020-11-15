1a18b4c4a787abb8ed99daec6a6d3f867cc230e7
Martin Citoler Saumell <martin.citoler@bigblueanalytics.com>
2020-11-15 10:42:32 +0000

    Added news tests for Solution's __getitem__

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -32,6 +32,33 @@ def test_solution_create():
     s[y] = 4.0
     assert m.addSol(s, free=True)
 
+def test_solution_evaluation():
+    m = Model()
+
+    x = m.addVar("x", lb=0, ub=2, obj=-1)
+    y = m.addVar("y", lb=0, ub=4, obj=0)
+    m.addCons(x*x <= y)
+
+    m.optimize()
+
+    sol = m.getBestSol()
+    
+    # Variable evaluation
+    assert round(sol[x]) == 2.0
+    assert round(sol[y]) == 4.0 
+
+    # Expression evaluation
+    expr = x*x + 2*x*y + y*y
+    expr2 = x + 1
+    assert round(sol[expr]) == 36.0
+    assert round(sol[expr2]) == 3.0
+
+    # Check consistency with Models's getVal method
+    assert sol[x] == m.getVal(x)
+    assert sol[y] == m.getVal(y)
+    assert sol[expr] == m.getVal(expr)
+    assert sol[expr2] == m.getVal(expr2)
+
 if __name__ == "__main__":
     test_solution_getbest()
     test_solution_create()
