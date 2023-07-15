32088eb45f600fe71f8e1af6af2e09fb2e81c2a4
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2023-07-15 10:09:10 +0200

    Make branch rule fundamental callbacks obligatory

---
+++ tests/test_branch_incomplete.py
@@ -0,0 +1,18 @@
+import pytest
+from pyscipopt import Model, Branchrule, SCIP_RESULT, quicksum, SCIP_PARAMSETTING
+
+
+def test_incomplete_branchrule():
+    class IncompleteBranchrule(Branchrule):
+        pass
+
+    branchrule = IncompleteBranchrule()
+    model = Model()
+    model.setPresolve(SCIP_PARAMSETTING.OFF)
+    model.setSeparating(SCIP_PARAMSETTING.OFF)
+    model.setHeuristics(SCIP_PARAMSETTING.OFF)
+    model.includeBranchrule(branchrule, "", "", priority=10000000, maxdepth=-1, maxbounddist=1)
+    model.readProblem("tests/data/10teams.mps.gz")
+
+    with pytest.raises(Exception):
+        model.optimize()
