a959785518900cef9c0a51802a8f0c3f1e73c38c
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2023-07-15 10:27:37 +0200

    Use crossplatform path join for instance path

--- tests/test_branch_incomplete.py
+++ tests/test_branch_incomplete.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from pyscipopt import Model, Branchrule, SCIP_RESULT, quicksum, SCIP_PARAMSETTING
 
 
@@ -12,7 +13,7 @@ def test_incomplete_branchrule():
     model.setSeparating(SCIP_PARAMSETTING.OFF)
     model.setHeuristics(SCIP_PARAMSETTING.OFF)
     model.includeBranchrule(branchrule, "", "", priority=10000000, maxdepth=-1, maxbounddist=1)
-    model.readProblem("tests/data/10teams.mps.gz")
+    model.readProblem(os.path.join("tests", "data", "10teams.mps.gz"))
 
     with pytest.raises(Exception):
         model.optimize()
