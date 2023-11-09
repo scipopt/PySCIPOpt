11d129bdd44e4bd0046bf820477f9cb9a0f5631d
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2023-11-09 15:10:30 +0100

    Trial

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ with open("README.md") as f:
 
 setup(
     name="PySCIPOpt",
-    version=version,
+    version="4.3.0",
     description="Python interface and modeling environment for SCIP",
     long_description=long_description,
     long_description_content_type="text/markdown",
