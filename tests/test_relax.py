d5cc41127941badc4010aebfc9a087b624e0fe1b
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2023-11-17 16:01:16 +0100

    Fix test_relax

--- tests/test_relax.py
+++ tests/test_relax.py
@@ -1,30 +1,33 @@
-from pyscipopt import Model
+from pyscipopt import Model, SCIP_RESULT
 from pyscipopt.scip import Relax
 
 calls = []
 
+
 class SoncRelax(Relax):
     def relaxexec(self):
         calls.append('relaxexec')
-        
+        return {'result': SCIP_RESULT.SUCCESS}
+
 
 def test_relax():
     m = Model()
     m.hideOutput()
 
-    #include relaxator
-    m.includeRelax(SoncRelax(),'testrelaxator','Test that relaxator gets included')
-    
-    #add Variables
-    x0 = m.addVar(vtype = "C", name = "x0")
-    x1 = m.addVar(vtype = "C", name = "x1")
-    x2 = m.addVar(vtype = "C", name = "x2")
-    
-    #addCons
+    # include relaxator
+    m.includeRelax(SoncRelax(), 'testrelaxator',
+                   'Test that relaxator gets included')
+
+    # add Variables
+    x0 = m.addVar(vtype="C", name="x0")
+    x1 = m.addVar(vtype="C", name="x1")
+    x2 = m.addVar(vtype="C", name="x2")
+
+    # addCons
     m.addCons(x0 >= 2)
     m.addCons(x0**2 <= x1)
     m.addCons(x1 * x2 >= x0)
-    
+
     m.setObjective(x1 + x0)
     m.optimize()
 
