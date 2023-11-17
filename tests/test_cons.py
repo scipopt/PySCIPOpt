c8219e833df42267fa6999b1fcc8893a73879027
Joao-Dionisio <up201606210@up.pt>
2023-11-17 15:14:32 +0000

    Add xfail. Code formatting.

--- tests/test_cons.py
+++ tests/test_cons.py
@@ -1,30 +1,33 @@
 from pyscipopt import Model, quicksum
 import random
+import pytest
 
 
 def test_getConsNVars():
-    n_vars = random.randint(100,1000)
+    n_vars = random.randint(100, 1000)
     m = Model()
     x = {}
     for i in range(n_vars):
-        x[i] = m.addVar("%i"%i)
-    
+        x[i] = m.addVar("%i" % i)
+
     c = m.addCons(quicksum(x[i] for i in x) <= 10)
     assert m.getConsNVars(c) == n_vars
 
     m.optimize()
     assert m.getConsNVars(c) == n_vars
 
+
 def test_getConsVars():
-    n_vars = random.randint(100,1000)
+    n_vars = random.randint(100, 1000)
     m = Model()
     x = {}
     for i in range(n_vars):
-        x[i] = m.addVar("%i"%i)
-    
+        x[i] = m.addVar("%i" % i)
+
     c = m.addCons(quicksum(x[i] for i in x) <= 1)
     assert m.getConsVars(c) == [x[i] for i in x]
-   
+
+
 def test_constraint_option_setting():
     m = Model()
     x = m.addVar()
@@ -41,6 +44,7 @@ def test_constraint_option_setting():
         assert c.isRemovable() == option
         assert c.isInitial() == option
 
+
 def test_cons_logical():
     m = Model()
     x1 = m.addVar(vtype="B")
@@ -50,24 +54,25 @@ def test_cons_logical():
     result1 = m.addVar(vtype="B")
     result2 = m.addVar(vtype="B")
 
-    m.addCons(x3 == 1-x1)
-    m.addCons(x4 == 1-x2)
-    
+    m.addCons(x3 == 1 - x1)
+    m.addCons(x4 == 1 - x2)
+
     # result1 true
-    m.addConsAnd([x1,x2], result1)
-    m.addConsOr([x1,x2], result1)
-    m.addConsXor([x1,x3], True)
+    m.addConsAnd([x1, x2], result1)
+    m.addConsOr([x1, x2], result1)
+    m.addConsXor([x1, x3], True)
 
     # result2 false
-    m.addConsOr([x3,x4], result2)
-    m.addConsAnd([x1,x3], result2)
-    m.addConsXor([x1,x2], False)
+    m.addConsOr([x3, x4], result2)
+    m.addConsAnd([x1, x3], result2)
+    m.addConsXor([x1, x2], False)
 
-    m.optimize() 
+    m.optimize()
 
-    assert m.getVal(result1) == 1
-    assert m.getVal(result2) == 0
-    
+    assert m.isEQ(m.getVal(result1), 1)
+    assert m.isEQ(m.getVal(result2), 0)
+
+
 def test_SOScons():
     m = Model()
     x = {}
@@ -85,48 +90,72 @@ def test_SOScons():
 
     m.optimize()
 
-    assert m.getVal(x[0]) == 0
-    assert m.getVal(x[1]) == 0
-    assert m.getVal(x[2]) == 0
-    assert m.getVal(x[3]) == 1
-    assert m.getVal(x[4]) == 1
-    assert m.getVal(x[5]) == 1
+    assert m.isEQ(m.getVal(x[0]), 0)
+    assert m.isEQ(m.getVal(x[1]), 0)
+    assert m.isEQ(m.getVal(x[2]), 0)
+    assert m.isEQ(m.getVal(x[3]), 1)
+    assert m.isEQ(m.getVal(x[4]), 1)
+    assert m.isEQ(m.getVal(x[5]), 1)
 
 
-# WIP. addConsIndicator doesn't behave as expected when binary variable is False. See Issue #717.
 def test_cons_indicator():
-    
     m = Model()
     x = m.addVar(lb=0)
-    binvar = m.addVar(vtype="B",lb=1)
-    #binvar2 = m.addVar(vtype="B", ub=0)
+    binvar = m.addVar(vtype="B", lb=1)
 
     c = m.addConsIndicator(x >= 1, binvar)
-    #c = m.addConsIndicator(x <= 0, binvar2)
 
     slack = m.getSlackVarIndicator(c)
 
     m.optimize()
 
-    assert m.getVal(slack) == 0
-    assert m.getVal(binvar) == 1
-    assert m.getVal(x) == 1
+    assert m.isEQ(m.getVal(slack), 0)
+    assert m.isEQ(m.getVal(binvar), 1)
+    assert m.isEQ(m.getVal(x), 1)
+
+
+# addConsIndicator doesn't behave as expected when binary variable is False. See Issue #717.
+@pytest.mark.xfail
+def test_cons_indicator_fail():
+    m = Model()
+    binvar = m.addVar(vtype="B")
+    x = m.addVar(vtype="C", lb=1, ub=3)
+    m.addConsIndicator(x <= 2, binvar)
+
+    m.setObjective(x, "maximize")
+
+    sol = m.createSol(None)
+    m.setSolVal(sol, x, 3)
+    m.setSolVal(sol, binvar, 0)
+    assert m.checkSol(sol)  # solution should be feasible
+
 
 def test_addConsCardinality():
     m = Model()
     x = {}
     for i in range(5):
-        x[i] = m.addVar(ub=1,obj=-1)
+        x[i] = m.addVar(ub=1, obj=-1)
 
     m.addConsCardinality([x[i] for i in range(5)], 3)
     m.optimize()
 
-    assert m.getVal(quicksum(x[i] for i in range(5))) == 3
+    assert m.isEQ(m.getVal(quicksum(x[i] for i in range(5))), 3)
+
 
 def test_printCons():
     m = Model()
     x = m.addVar()
     y = m.addVar()
-    c = m.addCons(x*y <= 5)
-    
+    c = m.addCons(x * y <= 5)
+
     m.printCons(c)
+
+
+@pytest.mark.skip
+def test_getValsLinear():
+    assert True
+
+
+@pytest.mark.skip
+def test_getRowLinear():
+    assert True
--- tests/test_solution.py
+++ tests/test_solution.py
@@ -1,12 +1,13 @@
 import pytest
 from pyscipopt import Model, scip, SCIP_PARAMSETTING, quicksum, quickprod
 
+
 def test_solution_getbest():
     m = Model()
 
     x = m.addVar("x", lb=0, ub=2, obj=-1)
     y = m.addVar("y", lb=0, ub=4, obj=0)
-    m.addCons(x*x <= y)
+    m.addCons(x * x <= y)
 
     m.optimize()
 
@@ -27,7 +28,7 @@ def test_solution_create():
 
     x = m.addVar("x", lb=0, ub=2, obj=-1)
     y = m.addVar("y", lb=0, ub=4, obj=0)
-    m.addCons(x*x <= y)
+    m.addCons(x * x <= y)
 
     s = m.createSol()
     s[x] = 2.0
@@ -36,8 +37,8 @@ def test_solution_create():
     assert m.addSol(s, free=True)
 
     s1 = m.createSol()
-    m.setSolVal(s1,x,1.0)
-    m.setSolVal(s1,y,2.0)
+    m.setSolVal(s1, x, 1.0)
+    m.setSolVal(s1, y, 2.0)
     assert m.checkSol(s1)
 
     m.optimize()
@@ -45,12 +46,13 @@ def test_solution_create():
     assert m.getSolObjVal(s1) == -1
     m.freeSol(s1)
 
+
 def test_solution_evaluation():
     m = Model()
 
     x = m.addVar("x", lb=0, ub=2, obj=-1)
     y = m.addVar("y", lb=0, ub=4, obj=0)
-    m.addCons(x*x <= y)
+    m.addCons(x * x <= y)
 
     m.optimize()
 
@@ -61,20 +63,21 @@ def test_solution_evaluation():
     assert round(sol[y]) == 4.0
 
     # Expression evaluation
-    expr = x*x + 2*x*y + y*y
+    expr = x * x + 2 * x * y + y * y
     expr2 = x + 1
     assert round(sol[expr]) == 36.0
     assert round(sol[expr2]) == 3.0
 
     # Check consistency with Models's getVal method
-    assert sol[x] == m.getVal(x)
-    assert m.getSolVal(sol,x) == m.getVal(x)
-    assert sol[y] == m.getVal(y)
-    assert m.getSolVal(sol,y) == m.getVal(y)
-    assert sol[expr] == m.getVal(expr)
-    assert m.getSolVal(sol,expr) == m.getVal(expr)
-    assert sol[expr2] == m.getVal(expr2)
-    assert m.getSolVal(sol,expr2) == m.getVal(expr2)
+    assert m.isEQ(sol[x], m.getVal(x))
+    assert m.isEQ(m.getSolVal(sol, x), m.getVal(x))
+    assert m.isEQ(sol[y], m.getVal(y))
+    assert m.isEQ(m.getSolVal(sol, y), m.getVal(y))
+    assert m.isEQ(sol[expr], m.getVal(expr))
+    assert m.isEQ(m.getSolVal(sol, expr), m.getVal(expr))
+    assert m.isEQ(sol[expr2], m.getVal(expr2))
+    assert m.isEQ(m.getSolVal(sol, expr2), m.getVal(expr2))
+
 
 def test_getSolTime():
     m = Model()
@@ -84,59 +87,64 @@ def test_getSolTime():
     for i in range(20):
         x[i] = m.addVar(ub=i)
 
-    for i in range(1,6):
-        m.addCons(quicksum(x[j] for j in range(20) if j%i==0) >= i)
-        m.addCons(quickprod(x[j] for j in range(20) if j%i==0) <= i**3)
-    
+    for i in range(1, 6):
+        m.addCons(quicksum(x[j] for j in range(20) if j % i == 0) >= i)
+        m.addCons(quickprod(x[j] for j in range(20) if j % i == 0) <= i**3)
+
     m.setObjective(quicksum(x[i] for i in range(20)))
     m.optimize()
     for s in m.getSols():
         assert m.getSolTime(s) >= 0
+
 
 def test_hasPrimalRay():
     m = Model()
     x = m.addVar()
     m.setObjective(x, "maximize")
     m.setPresolve(SCIP_PARAMSETTING.OFF)
-    
+
     m.optimize()
-    
+
     assert m.hasPrimalRay()
 
     m = Model()
-    x = m.addVar(lb = 0) # for readability
+    x = m.addVar(lb=0)  # for readability
     m.setPresolve(SCIP_PARAMSETTING.OFF)
 
     m.optimize()
 
     assert not m.hasPrimalRay()
-    
+
+
 def test_getPrimalRayVal():
     m = Model()
     x = m.addVar()
     m.setObjective(x, "maximize")
     m.setPresolve(SCIP_PARAMSETTING.OFF)
-    
+
     m.hideOutput()
     m.optimize()
-    
+
     assert m.getPrimalRayVal(x) == 1
-    
+
+
 def test_getPrimalRay():
     m = Model()
     x = m.addVar()
     y = m.addVar()
     m.setObjective(x, "maximize")
     m.setPresolve(SCIP_PARAMSETTING.OFF)
-    
+
     m.hideOutput()
     m.optimize()
 
-    assert m.getPrimalRay() == [1,0]
+    assert m.getPrimalRay() == [1, 0]
+
 
 def test_create_solution():
     with pytest.raises(ValueError):
         scip.Solution()
+
 
 def test_print_soltion():
     m = Model()
@@ -146,6 +154,7 @@ def test_print_soltion():
 
     assert str(m.getBestSol()) == "{'x1': -0.0}"
 
+
 def test_getSols():
     m = Model()
 
@@ -153,4 +162,4 @@ def test_getSols():
     m.optimize()
 
     assert len(m.getSols()) >= 1
-    assert any(sol[x] == 0.0 for sol in m.getSols())
+    assert any(m.isEQ(sol[x], 0.0) for sol in m.getSols())
