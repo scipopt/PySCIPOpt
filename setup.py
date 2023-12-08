f4f64baad69b930ca9bd99541f00e525c63a4347
Mark Turner <turner@zib.de>
2023-12-08 16:05:24 +0100

    Add new stup.py version

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ with open("README.md") as f:
 
 setup(
     name="PySCIPOpt",
-    version="4.3.0",
+    version="4.3.0.cibuildwheeltest",
     description="Python interface and modeling environment for SCIP",
     long_description=long_description,
     long_description_content_type="text/markdown",
