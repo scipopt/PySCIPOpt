97cbf8805331c4bbae3e11e4d4b309d1fe91b916
Julian Manns <manns@zib.de>
2024-03-05 12:31:48 +0100

    Undo skip test

--- tests/test_branch_incomplete.py
+++ tests/test_branch_incomplete.py
@@ -1,23 +1,19 @@
 import pytest
+import os
 from pyscipopt import Model, Branchrule, SCIP_PARAMSETTING
 
 
-@pytest.mark.skip(reason="fix later")
 def test_incomplete_branchrule():
     class IncompleteBranchrule(Branchrule):
         pass
 
     branchrule = IncompleteBranchrule()
     model = Model()
-    x = model.addVar(obj=-5, ub=100, vtype="INTEGER")
-    y = model.addVar(obj=-6, ub=100, vtype="INTEGER")
-    model.addCons(x + y <= 5)
-    model.addCons(4*x + 7*y <= 28)
-    model.includeBranchrule(branchrule, "", "", priority=99999999, maxdepth=-1, maxbounddist=1)
     model.setPresolve(SCIP_PARAMSETTING.OFF)
     model.setSeparating(SCIP_PARAMSETTING.OFF)
     model.setHeuristics(SCIP_PARAMSETTING.OFF)
-    model.disablePropagation()
+    model.includeBranchrule(branchrule, "", "", priority=10000000, maxdepth=-1, maxbounddist=1)
+    model.readProblem(os.path.join("tests", "data", "10teams.mps"))
 
     with pytest.raises(Exception):
         model.optimize()
