a82bfd59e5813743e8a50f89295b574f9a0fa268
Joao-Dionisio <up201606210@up.pt>
2024-04-01 13:17:07 +0100

    Fix segfault

--- tests/test_recipe_nonlinear.py
+++ tests/test_recipe_nonlinear.py
@@ -18,16 +18,35 @@ def test_nonlinear_objective():
     obj += z**3 * y
 
     model.addCons(v + w + x + y + z <= 1)
-    model2 = Model(sourceModel=model)
-
     set_nonlinear_objective(model, obj, sense='maximize')
-    t = model2.addVar("objective")
-    model2.addCons(t <= obj)
-    model2.setObjective(t, "maximize")
+
+    model2 = Model()
+
+    a = model2.addVar()
+    b = model2.addVar()
+    c = model2.addVar()
+    d = model2.addVar()
+    e = model2.addVar()
+
+    obj2 = 0
+    obj2 += exp(a)
+    obj2 += log(b)
+    obj2 += sqrt(c)
+    obj2 += sin(d)
+    obj2 += e**3 * d
+
+    model2.addCons(a + b + c + d + e <= 1)
+    
+    t = model2.addVar(lb=-float("inf"),obj=1)
+    model2.addCons(t <= obj2)
+    model2.setMaximize()
 
     obj_expr = model.getObjective()
     assert obj_expr.degree() == 1
 
+    model.setParam("numerics/epsilon", 10**(-5)) # bigger eps due to nonlinearities
+    model2.setParam("numerics/epsilon", 10**(-5)) 
+
     model.optimize()
     model2.optimize()
-    #assert model.isEQ(model.getObjVal(), model2.getObjVal())
+    assert model.isEQ(model.getObjVal(), model2.getObjVal())
