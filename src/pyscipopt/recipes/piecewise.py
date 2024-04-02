eff4bd04d2e26f6d7a36f22cb6cc865069d921be
Mohammed Ghannam <ghannam@zib.de>
2024-04-03 00:20:44 +0200

    Fix pipelines (#834)
    * Use backwards compatible type hint
    
    * Install bison in macos pipeline
    
    * Enable integration test for pull requests for now
    
    * Install locales needed by tests in ubuntu integration test
    
    * Turn off again integration tests for PRs

--- src/pyscipopt/recipes/piecewise.py
+++ src/pyscipopt/recipes/piecewise.py
@@ -1,9 +1,12 @@
+from typing import List
 
 from pyscipopt import Model, quicksum, Variable, Constraint
 
-def add_piecewise_linear_cons(model: Model, X: Variable, Y: Variable, a: list[float], b: list[float]) -> Constraint:
-        """add constraint of the form y = f(x), where f is a piecewise linear function
 
+def add_piecewise_linear_cons(model: Model, X: Variable, Y: Variable, a: List[float], b: List[float]) -> Constraint:
+    """add constraint of the form y = f(x), where f is a piecewise linear function
+
+            :param model: pyscipopt model to add the constraint to
             :param X: x variable
             :param Y: y variable
             :param a: array with x-coordinates of the points in the piecewise linear relation
@@ -12,25 +15,25 @@ def add_piecewise_linear_cons(model: Model, X: Variable, Y: Variable, a: list[fl
             Disclaimer: For the moment, can only model 2d piecewise linear functions
             Adapted from https://github.com/scipopt/PySCIPOpt/blob/master/examples/finished/piecewise.py
         """
-        assert len(a) == len(b), "Must have the same number of x and y-coordinates"
+    assert len(a) == len(b), "Must have the same number of x and y-coordinates"
 
-        K = len(a)-1
-        w,z = {},{}
-        for k in range(K):
-            w[k] = model.addVar(lb=-model.infinity()) 
-            z[k] = model.addVar(vtype="B")
+    K = len(a) - 1
+    w, z = {}, {}
+    for k in range(K):
+        w[k] = model.addVar(lb=-model.infinity())
+        z[k] = model.addVar(vtype="B")
 
-        for k in range(K):
-            model.addCons(w[k] >= a[k]*z[k])
-            model.addCons(w[k] <= a[k+1]*z[k])
+    for k in range(K):
+        model.addCons(w[k] >= a[k] * z[k])
+        model.addCons(w[k] <= a[k + 1] * z[k])
 
-        model.addCons(quicksum(z[k] for k in range(K)) == 1)
+    model.addCons(quicksum(z[k] for k in range(K)) == 1)
 
-        model.addCons(X == quicksum(w[k] for k in range(K)))
+    model.addCons(X == quicksum(w[k] for k in range(K)))
 
-        c = [float(b[k+1]-b[k]) / (a[k+1]-a[k]) for k in range(K)]
-        d = [b[k] - c[k]*a[k] for k in range(K)]
-        
-        new_cons = model.addCons(Y == quicksum(d[k]*z[k] + c[k]*w[k] for k in range(K)))
-        
-        return new_cons
+    c = [float(b[k + 1] - b[k]) / (a[k + 1] - a[k]) for k in range(K)]
+    d = [b[k] - c[k] * a[k] for k in range(K)]
+
+    new_cons = model.addCons(Y == quicksum(d[k] * z[k] + c[k] * w[k] for k in range(K)))
+
+    return new_cons
