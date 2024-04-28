92061caf89d6d6b29812e1df9619815787942515
Joao-Dionisio <up201606210@up.pt>
2024-04-28 19:02:27 +0100

    Fix path

--- tests/test_model.py
+++ tests/test_model.py
@@ -361,6 +361,6 @@ def test_locale():
     with open("model.cip") as file:
         assert "1,1" not in file.read()
         
-    m.readProblem("data/test_locale.cip")
+    m.readProblem(os.path.join("tests", "data", "test_locale.cip"))
 
     locale.setlocale(locale.LC_NUMERIC,"")
