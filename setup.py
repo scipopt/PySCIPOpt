9809421e54bfa0cd138254d057413f8709a9dcc2
Mark Turner <turner@zib.de>
2024-06-21 15:22:35 +0200

    Change version to 5.1.0

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ with open("README.md") as f:
 
 setup(
     name="PySCIPOpt",
-    version="5.0.1",
+    version="5.1.0",
     description="Python interface and modeling environment for SCIP",
     long_description=long_description,
     long_description_content_type="text/markdown",
