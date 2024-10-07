0a0e45493f2ce50a70150599b6c4b6f8c8cb25a5
Mark Turner <64978342+Opt-Mucca@users.noreply.github.com>
2024-10-07 12:00:30 +0200

    Update docs/conf.py
    Co-authored-by: Mohammed Ghannam <ghannam@zib.de>

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,6 @@ extlinks = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = "sphinx_nefertiti"
 html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
