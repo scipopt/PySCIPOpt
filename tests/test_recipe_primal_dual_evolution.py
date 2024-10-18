53627d21c88b5dcdf20da9c597a41ab234823984
Joao-Dionisio <up201606210@up.pt>
2024-10-18 13:33:57 +0200

    More robust testing

--- tests/test_recipe_primal_dual_evolution.py
+++ tests/test_recipe_primal_dual_evolution.py
@@ -18,3 +18,19 @@ def test_primal_dual_evolution():
     # these are required because the event handler doesn't capture the final state
     model.data["primal_solutions"].append((model.getSolvingTime(), model.getPrimalbound()))
     model.data["dual_solutions"].append((model.getSolvingTime(), model.getDualbound()))
+
+    for i in range(1, len(model.data["primal_solutions"])):
+        if model.getObjectiveSense() == "minimize":
+            assert model.data["primal_solutions"][i][1] <= model.data["primal_solutions"][i-1][1]
+        else:
+            assert model.data["primal_solutions"][i][1] >= model.data["primal_solutions"][i-1][1]
+    
+    for i in range(1, len(model.data["dual_solutions"])):
+        if model.getObjectiveSense() == "minimize":
+            assert model.data["dual_solutions"][i][1] >= model.data["dual_solutions"][i-1][1]
+        else:
+            assert model.data["dual_solutions"][i][1] <= model.data["dual_solutions"][i-1][1]
+
+    # how to get a simple plot of the data
+    #from pyscipopt.recipes.primal_dual_evolution import plot_primal_dual_evolution
+    #plot_primal_dual_evolution(model)
