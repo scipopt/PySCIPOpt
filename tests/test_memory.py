7c87182fd60a99928f11bf36eb09a72524fc9239
Dionisio <jdionisi@opt130.zib.de>
2024-10-28 15:16:10 +0100

    remove is_optimized_mode

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,15 +1,14 @@
 import pytest
 from pyscipopt.scip import Model, is_memory_freed, print_memory_in_use
-from helpers.utils import is_optimized_mode
 
 def test_not_freed():
-    if is_optimized_mode():
+    if is_memory_freed():
        pytest.skip()
     m = Model()
     assert not is_memory_freed()
 
 def test_freed():
-    if is_optimized_mode():
+    if is_memory_freed():
        pytest.skip()
     m = Model()
     del m
