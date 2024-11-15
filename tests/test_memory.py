15f7e1bcfd8802060b08442c5aaa51a58b2eb4c0
Joao-Dionisio <up201606210@up.pt>
2024-11-15 15:28:52 +0100

    finally working

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -2,13 +2,13 @@ import pytest
 from pyscipopt.scip import Model, is_memory_freed, print_memory_in_use
 
 def test_not_freed():
-    if is_memory_freed():
+    if is_optimized_mode():
        pytest.skip()
     m = Model()
     assert not is_memory_freed()
 
 def test_freed():
-    if is_memory_freed():
+    if is_optimized_mode():
        pytest.skip()
     m = Model()
     del m
