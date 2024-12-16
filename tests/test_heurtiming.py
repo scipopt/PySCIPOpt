9e8c96186c86a9171ee18f19b5b7ec22e03f3098
Joao-Dionisio <up201606210@up.pt>
2024-12-16 11:00:42 +0100

    delete duplicate test, add CHANGELOG

--- tests/test_heurtiming.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from pyscipopt import Model, SCIP_HEURTIMING
-
-def test_heurTiming():
-    model = Model()
-    model.setHeurTiming('rins', SCIP_HEURTIMING.BEFORENODE)
-    print("timing of rins: %d\n" % model.getHeurTiming('rins'))
