973970bcb03c615fb18cc52e02dd6d0fe9c98a81
Dominik Kamp <dominik.kamp@uni-bayreuth.de>
2025-05-06 15:04:13 +0200

    Remove redundant delimiter

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ extra_link_args = []
 # if SCIPOPTDIR is not set, we assume that SCIP is installed globally
 if not scipoptdir:
     if platform.system() == "Darwin":
-        includedir = "/usr/local/include/"
-        libdir = "/usr/local/lib/"
+        includedir = "/usr/local/include"
+        libdir = "/usr/local/lib"
     else:
         includedir = "."
         libdir = "."
