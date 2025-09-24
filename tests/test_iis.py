3b7fab317e7ed8312864012e80d788c17e356d9a
Mohammed Ghannam <mghannam@zib.de>
2025-09-24 12:18:57 +0200

    Add simple iis tests

--- tests/test_iis.py
+++ tests/test_iis.py
@@ -2,41 +2,39 @@ import pytest
 
 from pyscipopt import Model, IISfinder
 
-class myIIS(IISfinder):
-    def __init__(self):
-        super().__init__()
-        self._iisfinder = None
-
-    def isIISFound(self):
-        return self._iisfinder is not None
-
-def test_iis_greedy_make_irreducible():
+def infeasible_model():
     m = Model()
-    x1 = m.addVar("x1")
-    x2 = m.addVar("x2")
-    x3 = m.addVar("x3")
+    x1 = m.addVar("x1", lb=0, ub=1, vtype="B")
+    x2 = m.addVar("x2", lb=0, ub=1, vtype="B")
+    x3 = m.addVar("x3", lb=0, ub=1, vtype="B")
 
-    m.addCons(x1 + x2 >= 5)
-    m.addCons(x2 + x3 >= 5)
-    m.addCons(x1 + x3 <= 3)
+    m.addCons(x1 + x2 == 1)
+    m.addCons(x2 + x3 == 1)
+    m.addCons(x1 + x3 == 1)
 
-    iisfinder = IISfinder()
+    return m
 
-    m.iisGreedyMakeIrreducible(iisfinder)
+def test_generate_iis():
+    m = infeasible_model()
 
-    assert iisfinder.isIISFound() == True
+    # make sure IIS generation doesn't raise any exceptions
+    m.generateIIS()
 
-def test_custom_iis():
-    m = Model()
-    x1 = m.addVar("x1")
-    x2 = m.addVar("x2")
-    x3 = m.addVar("x3")
 
-    m.addCons(x1 + x2 >= 5)
-    m.addCons(x2 + x3 >= 5)
-    m.addCons(x1 + x3 <= 3)
+def test_custom_iis_finder():
+    class MyIIS(IISfinder):
+        def __init__(self):
+            super().__init__()
+            self._iisfinder = None
+        
+    
+    m = infeasible_model()
+    my_iis = MyIIS()
 
-    iisfinder = myIIS()
-
-    pass
+    m.includeIISfinder(my_iis, "", "")
+    
+    # should raise an exception since the custom IIS finder doesn't implement the exec method
+    with pytest.raises(Exception):
+        m.generateIIS()
+    
 
