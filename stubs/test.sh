813121c1adb33c43170f8a701cc03942a10306e6
Jonathan Berthias <jvberthias@gmail.com>
2025-12-28 21:23:15 +0100

    Clarify requirements for stubtest

--- stubs/test.sh
+++ stubs/test.sh
@@ -2,9 +2,9 @@
 
 # Test the stubs for pyscipopt using stubtest
 # This checks that the type hints in the stubs are consistent with the actual implementation
-# Prerequisite: install mypy in same environment as pyscipopt and put stubtest in PATH
+# Prerequisite: install mypy (which provides stubtest) in the same environment as pyscipopt
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-stubtest \
+python -m mypy.stubtest \
   --allowlist "$SCRIPT_DIR/allowlist" \
   --allowlist "$SCRIPT_DIR/todo" \
   pyscipopt
