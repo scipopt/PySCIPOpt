5f22e2ee7beabb47e872e98e57d9fc713c39acf8
mymissuniverse <mymissuniverse@icloud.com>
2026-01-19 01:29:44 +0900

    Add structured_optimization_trace recipe and tests

---
+++ src/pyscipopt/recipes/structured_optimization_trace.py
@@ -0,0 +1,37 @@
+from pyscipopt import SCIP_EVENTTYPE, Eventhdlr, Model
+
+
+def attach_structured_optimization_trace(model: Model):
+    """
+    Attaches an event handler that records optimization progress in structured JSONL format.
+
+    Args:
+        model: SCIP Model
+    """
+
+    class TraceEventhdlr(Eventhdlr):
+        def eventinit(self):
+            self.model.catchEvent(SCIP_EVENTTYPE.BESTSOLFOUND, self)
+            self.model.catchEvent(SCIP_EVENTTYPE.DUALBOUNDIMPROVED, self)
+
+        def eventexec(self, event):
+            record = {
+                "time": self.model.getSolvingTime(),
+                "primalbound": self.model.getPrimalbound(),
+                "dualbound": self.model.getDualbound(),
+                "gap": self.model.getGap(),
+                "nodes": self.model.getNNodes(),
+                "nsol": self.model.getNSols(),
+            }
+            self.model.data["trace"].append(record)
+
+    if not hasattr(model, "data") or model.data is None:
+        model.data = {}
+    model.data["trace"] = []
+
+    hdlr = TraceEventhdlr()
+    model.includeEventhdlr(
+        hdlr, "structured_trace", "Structured optimization trace handler"
+    )
+
+    return model
