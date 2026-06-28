aecf12ed43e9c917aaaa87662e4f83d01c3ffa32
Jonathan Berthias <jvberthias@gmail.com>
2026-06-28 13:41:38 +0200

    Improve baseline script

--- stubs/baseline.sh
+++ stubs/baseline.sh
@@ -9,5 +9,6 @@ python "$REPO_ROOT"/scripts/generate_expr_type_tests.py
 for test_file in "$REPO_ROOT"/tests/@types/*.py; do
     echo "Updating mypy baseline for $test_file"
     output_file="${test_file%.*}.mypy.out"
-    python -m mypy "$test_file" --warn-unused-ignores | grep "error:" > "$output_file"
+    python -m mypy "$test_file" --warn-unused-ignores | grep "error:" > /tmp/pyscipopt-mypy-output.txt
+    cp /tmp/pyscipopt-mypy-output.txt "$output_file"
 done
