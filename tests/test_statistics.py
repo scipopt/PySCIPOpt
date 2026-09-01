6ba6467781e89eab5b6194dce682ff641021b50f
Adam Jemielita <trashci18@gmail.com>
2026-09-01 19:05:08 +0200

    refactor statistics tests

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,3 +1,4 @@
+from pyscipopt.scip import Model
 import os
 from helpers.utils import random_mip_1
 from json import load
@@ -7,8 +8,31 @@ import numpy as np
 
 @pytest.fixture
 def optimized_model():
-    model = random_mip_1(small=True, node_lim=2400)  # Using small=True for speed across tests
+    # Using small=True for speed across tests
+    # finds 1 primal solution
+    model = random_mip_1(small=True, node_lim=2400)
     model.optimize()
+    return model
+
+
+# very simple model with 2 primal solutions
+# created because getting more than 1 primal solution from random_mip_1 requres setting a large node limit, which slows down the tests
+@pytest.fixture
+def optimized_model_with_primal_solutions():
+    model = Model()
+
+    x = model.addVar(vtype="I", lb=0, ub=5, name="x")
+    y = model.addVar(vtype="I", lb=0, ub=5, name="y")
+    z = model.addVar(vtype="I", lb=0, ub=5, name="z")
+
+    model.addCons(x + y + z <= 5)
+    model.addCons(x >= 1)
+    model.addCons(y >= 1)
+
+    model.setObjective(x + y + z, "maximize")
+
+    model.optimize()
+
     return model
 
 
@@ -22,10 +46,14 @@ def test_statistics_json(optimized_model):
     os.remove("statistics.json")
 
 
-def test_getNSolsFound(optimized_model):
+def test_getNSolsFound(optimized_model, optimized_model_with_primal_solutions):
     sols = optimized_model.getNSolsFound()
 
     assert sols >= 1
+
+    sols_model_with_primals = optimized_model_with_primal_solutions.getNSolsFound()
+
+    assert sols_model_with_primals >= 2
 
 
 def test_getPrimalDualIntegral(optimized_model):
@@ -85,11 +113,12 @@ def test_getAvgLowerbound(optimized_model):
     manual_avg_lowerbound = 0.0
     if len(open_nodes) > 0:
         manual_avg_lowerbound = np.mean(
-            [node.getLowerbound() for node in open_nodes] + [optimized_model.getFocusNode().getLowerbound()]
+            [node.getLowerbound() for node in open_nodes]
+            + [optimized_model.getFocusNode().getLowerbound()]
         )
 
     assert isinstance(avg_lowerbound, float)
-    assert manual_avg_lowerbound == pytest.approx(avg_lowerbound)
+    assert optimized_model.isEQ(manual_avg_lowerbound, avg_lowerbound)
 
 
 def test_getAvgDualbound(optimized_model):
@@ -97,7 +126,10 @@ def test_getAvgDualbound(optimized_model):
     avg_lowerbound = optimized_model.getAvgLowerbound()
 
     assert isinstance(avg_dualbound, float)
-    assert avg_dualbound == pytest.approx(avg_lowerbound) or avg_dualbound == pytest.approx(-avg_lowerbound)
+    assert (
+        optimized_model.isEQ(avg_dualbound, avg_lowerbound)
+        or optimized_model.isEQ(avg_dualbound, -avg_lowerbound)
+    )
 
 
 def test_getDeterministicTime(optimized_model):
@@ -112,15 +144,15 @@ def test_getUpperbound(optimized_model):
     lowerbound = optimized_model.getLowerbound()
 
     assert isinstance(upperbound, float)
-    assert upperbound >= lowerbound
+    assert optimized_model.isGE(upperbound, lowerbound)
 
 
-def test_getFirstPrimalBound(optimized_model):
-    first_primal = optimized_model.getFirstPrimalBound()
-    upperbound = optimized_model.getUpperbound()
+def test_getFirstPrimalBound(optimized_model_with_primal_solutions):
+    first_primal = optimized_model_with_primal_solutions.getFirstPrimalBound()
+    upperbound = optimized_model_with_primal_solutions.getUpperbound()
 
     assert isinstance(first_primal, float)
-    assert first_primal >= upperbound
+    assert optimized_model_with_primal_solutions.isGT(first_primal, upperbound)
 
 
 def test_getLowerboundRoot(optimized_model):
@@ -128,4 +160,4 @@ def test_getLowerboundRoot(optimized_model):
     lowerbound = optimized_model.getLowerbound()
 
     assert isinstance(lowerbound_root, float)
-    assert lowerbound_root <= lowerbound
+    assert optimized_model.isLE(lowerbound_root, lowerbound)
