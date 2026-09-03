5139b576fa7505917458c1f9c5782608a0997f8b
Adam Jemielita <trashci18@gmail.com>
2026-09-03 17:22:51 +0200

    improve statistics tests

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -9,31 +9,39 @@ import numpy as np
 @pytest.fixture
 def optimized_model():
     # Using small=True for speed across tests
-    # finds 1 primal solution
     model = random_mip_1(small=True, node_lim=2400)
     model.optimize()
     return model
 
 
-# very simple model with 2 primal solutions
-# created because getting more than 1 primal solution from random_mip_1 requres setting a large node limit, which slows down the tests
+# model factory for testing solution-related statistics
 @pytest.fixture
-def optimized_model_with_primal_solutions():
-    model = Model()
+def make_optimized_model_with_fixed_primal_solutions():
+    def _make(solutions):
+        model = Model()
 
-    x = model.addVar(vtype="I", lb=0, ub=5, name="x")
-    y = model.addVar(vtype="I", lb=0, ub=5, name="y")
-    z = model.addVar(vtype="I", lb=0, ub=5, name="z")
+        x = model.addVar(vtype="I", lb=0, ub=2)
+        y = model.addVar(vtype="I", lb=0, ub=2)
+        z = model.addVar(vtype="I", lb=0, ub=2)
 
-    model.addCons(x + y + z <= 5)
-    model.addCons(x >= 1)
-    model.addCons(y >= 1)
+        model.addCons(x + y + z <= 2)
 
-    model.setObjective(x + y + z, "maximize")
+        model.setObjective(x + y + z, "maximize")
 
-    model.optimize()
+        for solution in solutions:
+            sol = model.createOrigSol()
 
-    return model
+            x_val, y_val, z_val = solution
+            sol[x] = x_val
+            sol[y] = y_val
+            sol[z] = z_val
+
+            model.addSol(sol)
+
+        model.optimize()
+        return model
+    
+    return _make
 
 
 def test_statistics_json(optimized_model):
@@ -46,14 +54,36 @@ def test_statistics_json(optimized_model):
     os.remove("statistics.json")
 
 
-def test_getNSolsFound(optimized_model, optimized_model_with_primal_solutions):
-    sols = optimized_model.getNSolsFound()
+def test_getNSolsFound(make_optimized_model_with_fixed_primal_solutions):
+    all_feasible_solutions = [
+        (0, 0, 0),
+        (1, 0, 0),
+        (0, 1, 0),
+        (0, 0, 1),
+        (1, 1, 0),
+        (1, 0, 1),
+        (0, 1, 1),
+        (2, 0, 0),
+        (0, 2, 0),
+        (0, 0, 2)
+    ]
+    full_model = make_optimized_model_with_fixed_primal_solutions(all_feasible_solutions)
+    n_sols_found = full_model.getNSolsFound()
 
-    assert sols >= 1
+    # here we now the exact number of feasible soutions, none will be found during optimization
+    assert n_sols_found == len(all_feasible_solutions)  
 
-    sols_model_with_primals = optimized_model_with_primal_solutions.getNSolsFound()
+    # Test with a subset of (non-optimal) feasible solutions
+    feasible_solutions = [
+        (0, 0, 0),
+        (1, 0, 0),
+        (0, 1, 0),
+        (0, 0, 1),
+    ]
 
-    assert sols_model_with_primals >= 2
+    subset_model = make_optimized_model_with_fixed_primal_solutions(feasible_solutions)
+    # none of the provided feasible solutions is optimal, so we expect at least one more solution to be found during optimization
+    assert subset_model.getNSolsFound() >= len(feasible_solutions) + 1
 
 
 def test_getPrimalDualIntegral(optimized_model):
@@ -80,14 +110,16 @@ def test_getNObjlimLeaves(optimized_model):
     n_objlim_leaves = optimized_model.getNObjlimLeaves()
 
     assert isinstance(n_objlim_leaves, int)
+    assert n_objlim_leaves >= 0
 
 
 def test_addNNodes(optimized_model):
+    n_nodes_to_add = 5
     initial_n_nodes = optimized_model.getNTotalNodes()
-    optimized_model.addNNodes(5)
+    optimized_model.addNNodes(n_nodes_to_add)
     new_n_nodes = optimized_model.getNTotalNodes()
 
-    assert new_n_nodes == initial_n_nodes + 5
+    assert new_n_nodes == initial_n_nodes + n_nodes_to_add
 
 
 def test_getMaxTotalDepth(optimized_model):
@@ -110,12 +142,10 @@ def test_getAvgLowerbound(optimized_model):
     avg_lowerbound = optimized_model.getAvgLowerbound()
     leaves, children, siblings = optimized_model.getOpenNodes()
     open_nodes = leaves + children + siblings
-    manual_avg_lowerbound = 0.0
-    if len(open_nodes) > 0:
-        manual_avg_lowerbound = np.mean(
-            [node.getLowerbound() for node in open_nodes]
-            + [optimized_model.getFocusNode().getLowerbound()]
-        )
+    manual_avg_lowerbound = np.mean(
+        [node.getLowerbound() for node in open_nodes]
+        + [optimized_model.getFocusNode().getLowerbound()]
+    )
 
     assert isinstance(avg_lowerbound, float)
     assert optimized_model.isEQ(manual_avg_lowerbound, avg_lowerbound)
@@ -147,12 +177,21 @@ def test_getUpperbound(optimized_model):
     assert optimized_model.isGE(upperbound, lowerbound)
 
 
-def test_getFirstPrimalBound(optimized_model_with_primal_solutions):
-    first_primal = optimized_model_with_primal_solutions.getFirstPrimalBound()
-    upperbound = optimized_model_with_primal_solutions.getUpperbound()
+def test_getFirstPrimalBound(make_optimized_model_with_fixed_primal_solutions):
+    # Test with a subset of (non-optimal) feasible solutions
+    feasible_solutions = [
+        (1, 0, 0),
+        (0, 1, 0),
+        (0, 0, 1),
+    ]
+    model = make_optimized_model_with_fixed_primal_solutions(feasible_solutions)
+    first_primalbound = model.getFirstPrimalBound()
+    primalbound = model.getPrimalbound()
 
-    assert isinstance(first_primal, float)
-    assert optimized_model_with_primal_solutions.isGT(first_primal, upperbound)
+    assert isinstance(first_primalbound, float)
+    # SCIP will evaluate provided feasible solutions first and find the first feasible solution with objective value equal to 1
+    # subsequently, SCIP will find the optimal solution with objective value equal to 2, which is the upper bound of the model
+    assert model.isLT(first_primalbound, primalbound)
 
 
 def test_getLowerboundRoot(optimized_model):
